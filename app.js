f7a841cce823f80822e6eb51d3718dd19c172e9a
derpy2289 <79153210+derpy2289@users.noreply.github.com>
2024-10-26 00:27:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function alertButton() {
     
 }
+const input = document.getElementById("number");
+const input = document.getElementById("number");
+const inputValue = input.value;
+RanNumber = Math.floor(Math.random() * 10);
+console.log(inputValue);
