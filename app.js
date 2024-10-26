557f7058ef791b3834d17337a3625d51365853b2
derpy2289 <79153210+derpy2289@users.noreply.github.com>
2024-10-26 00:34:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 function alertButton() {
     
 }
-const input = document.getElementById("number");
-const input = document.getElementById("number");
-const inputValue = input.value;
-RanNumber = Math.floor(Math.random() * 10);
-console.log(inputValue);
+function Random_Number() {
+    const input = document.getElementById("number");
+    const inputValue = input.value;
+    RanNumber = Math.floor(Math.random() * 10);
+    if (inputValue == RanNumber) {
+        return "YOU GEUSSED THE RIGHT NUMBER!!! YIPEEE"
+    } else {
+        return "HAHAHHAHA LOSER TRY AGAIN"
+    }
+}
