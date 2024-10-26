e0b14b7690a36daa557435bd1e5beb978e041516
derpy2289 <79153210+derpy2289@users.noreply.github.com>
2024-10-26 00:42:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ function Random_Number() {
     const input = document.getElementById("number");
     const inputValue = input.value;
     RanNumber = Math.floor(Math.random() * 10);
-    if (inputValue == RanNumber) {
-        document.getElementById('output').innerText = "YOU GEUSSED THE RIGHT NUMBER!!! YIPEEE"
+    if (inputValue > 10) {
+        document.getElementById('output').innerText = "ARE YOU A FUCKING IDIOT THIS IS NOT A NUMBER BETWEEN 1 AND 10"
     } else {
-        document.getElementById('output').innerText = "HAHAHHAHA LOSER TRY AGAIN"
+        if (inputValue < 0) {
+            document.getElementById('output').innerText = "ARE YOU A FUCKING IDIOT THIS IS NOT A NUMBER BETWEEN 1 AND 10"
+    } else {
+        if (inputValue == RanNumber) {
+            document.getElementById('output').innerText = "YOU GEUSSED THE RIGHT NUMBER!!! YIPEEE"
+        } else {
+            document.getElementById('output').innerText = "HAHAHHAHA LOSER TRY AGAIN"
     }
 }
