449795c9b7142c65cee0772c10b63a6c55055241
derpy2289 <79153210+derpy2289@users.noreply.github.com>
2024-10-26 00:50:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-function alertButton() {
-    
-}
 function Random_Number() {
     const input = document.getElementById("number");
     const inputValue = input.value;
-    RanNumber = Math.floor(Math.random() * 10);
-    if (inputValue > 10) {
-        document.getElementById('output').innerText = "ARE YOU A FUCKING IDIOT THIS IS NOT A NUMBER BETWEEN 1 AND 10"
+    const RanNumber = Math.floor(Math.random() * 10);
+    
+    if (inputValue > 10 || inputValue < 0) {
+        document.getElementById('output').innerText = "ARE YOU A FUCKING IDIOT THIS IS NOT A NUMBER BETWEEN 1 AND 10";
+    } else if (inputValue == RanNumber) {
+        document.getElementById('output').innerText = "YOU GUESSED THE RIGHT NUMBER!!! YIPEEE";
     } else {
-        if (inputValue < 0) {
-            document.getElementById('output').innerText = "ARE YOU A FUCKING IDIOT THIS IS NOT A NUMBER BETWEEN 1 AND 10"
-    } else {
-        if (inputValue == RanNumber) {
-            document.getElementById('output').innerText = "YOU GEUSSED THE RIGHT NUMBER!!! YIPEEE"
-        } else {
-            document.getElementById('output').innerText = "HAHAHHAHA LOSER TRY AGAIN"
+         document.getElementById('output').innerText = "HAHAHHAHA LOSER TRY AGAIN";
     }
 }
