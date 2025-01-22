723aa2eb06bbece67ea8167b741bcee480c2f4d1
derpy2289 <79153210+derpy2289@users.noreply.github.com>
2025-01-22 00:09:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function Random_Number() {
     const RanNumber = Math.floor(Math.random() * 10) + 1;
 
     
-    if (input == 'e') {
+    if (input == e) {
         document.getElementById("output").innerText = "DONT PUT A FUCKING e or E. ARE YOU JACKSHIT CRAZY WTF ARE YOU DOING WITH YOUR LIFE GO DO SMTH PRODUCTIVE LIKE IDK TRANSLATE THIS MORSE CODE . . . - - - . . . - - - . . . - - - . . . - - MAYBE PLAYING WITH SOUND OR PUT IT INTO A TRANSLATOR WILL HELP GL";
     } else if (inputValue > 10 || inputValue <= 0) {
         document.getElementById("output").innerText = "ARE YOU A FUCKING IDIOT THIS IS NOT A NUMBER BETWEEN 1 AND 10";
