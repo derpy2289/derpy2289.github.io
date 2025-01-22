8ce37349eec2536e62e9670503f4748e43cde65c
derpy2289 <79153210+derpy2289@users.noreply.github.com>
2025-01-21 23:28:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function Random_Number() {
     const inputValue = input.value;
     const RanNumber = Math.floor(Math.random() * 10);
     
-    if (inputValue > 10 || inputValue < 0) {
+    if (inputValue > 10 || inputValue <= 0) {
         document.getElementById("output").innerText = "ARE YOU A FUCKING IDIOT THIS IS NOT A NUMBER BETWEEN 1 AND 10";
     } else if (inputValue == RanNumber) {
         document.getElementById("output").innerText = "YOU GUESSED THE RIGHT NUMBER!!! YIPEEE, THE NUMBER WAS " + RanNumber;
